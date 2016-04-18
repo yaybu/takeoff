0b4d88c1aa56e074379728ad668553af240cabce
John Carr <john.carr@unrouted.co.uk>
2016-04-18 07:16:03 +0100

    Automatic generation of keypairs

--- takeoff/aws/environment.py
+++ takeoff/aws/environment.py
@@ -1,5 +1,6 @@
 from touchdown.core.resource import Resource
 from touchdown.core import argument, plan
+from touchdown.config import expressions
 
 from .account import Account
 
@@ -19,12 +20,24 @@ class BuildWorkspace(plan.Plan):
     resource = Environment
 
     def setup(self):
+        workspace = self.runner.get_service(self.resource.account.workspace, self.name)
         parent = self.runner.get_service(self.resource.parent, self.name)
+
         self.keypair = parent.aws.add_keypair(
             name=self.resource.name,
+            private_key=workspace.project_config.add_string(
+                name="secrets.keypair",
+                default=expressions.rsa_private_key(),
+                retain_default=True,
+            )
         )
 
         self.vpc = parent.aws.add_vpc(
             name=self.resource.name,
             cidr_block=str(self.resource.cidr_block),
+        )
+
+        self.allocations = workspace.project_config.add_ip_allocations(
+            name=self.resource.name + ".subnets",
+            network=str(self.resource.cidr_block),
         )
